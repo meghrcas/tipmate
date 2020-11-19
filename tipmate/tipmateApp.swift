e9bdb6ae63317b3c2e3922b737071d2115934cc0
meghrcas <meghanrcastro@gmail.com>
2020-11-19 04:12:58 -0500

    Initial Commit

---
+++ tipmate/tipmateApp.swift
@@ -0,0 +1,17 @@
+//
+//  tipmateApp.swift
+//  tipmate
+//
+//  Created by Meghan Castro on 11/19/20.
+//
+
+import SwiftUI
+
+@main
+struct tipmateApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
